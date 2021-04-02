b32bea3c1db9fc14e238c01992854cc1f5d24c61
Jooseop Kim <65662520+js0221@users.noreply.github.com>
2021-04-03 06:17:30 +0900

    Update 177.sql

--- SQL/leetcode/177.sql
+++ SQL/leetcode/177.sql
@@ -11,18 +11,37 @@ RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-      SELECT CASE WHEN COUNT(sub.Salary) < N THEN NULL
-                  ELSE MIN(sub.Salary)
-             END
-      FROM(
-            SELECT DISTINCT Salary
-            FROM Employee
-            ORDER BY Salary DESC
-            LIMIT N
-            ) sub
+     SELECT CASE WHEN COUNT(sub.Salary) < N THEN NULL
+                 ELSE MIN(sub.Salary)
+            END
+     FROM(
+          SELECT DISTINCT Salary
+          FROM Employee
+          ORDER BY Salary DESC
+          LIMIT N
+          ) sub
   );
 END
 
 -- 풀이 2: IF함수
+/*
+IF function : IF(condition, value_if_true, value_if_false)
+풀이 1의 case를 if로 변경
+*/
+
+CREATE FUNCTION getNthHighestSalary(N INT)
+RETURNS INT
+BEGIN
+  RETURN (
+     SELECT IF(COUNT(sub.Salary) < N, NULL, MIN(sub.Salary))
+     FROM(
+          SELECT DISTINCT Salary
+          FROM Employee
+          ORDER BY Salary DESC
+          LIMIT N
+          ) sub
+  );
+END
+
 
 -- 풀이 3: LIMIT, OFFSET
