83034412fb3a26193a15948a448a1b42290498d5
Jooseop Kim <65662520+js0221@users.noreply.github.com>
2021-04-03 06:40:29 +0900

    Update 177.sql

--- SQL/leetcode/177.sql
+++ SQL/leetcode/177.sql
@@ -3,7 +3,6 @@
 2. 조건이 하나일때, IF문 사용, 조건이 여러개일때, CASE문 사용
 */
 
-
 -- 풀이 1: CASE문, 서브쿼리
 /* 
 1. LIMIT로 셀러리를 N개만 가져와서 가장 마지막에 있는 것을 선택
@@ -27,7 +26,6 @@ BEGIN
   );
 END
 
-
 -- 풀이 2: IF함수
 /*
 IF function : IF(condition, value_if_true, value_if_false)
@@ -48,7 +46,6 @@ BEGIN
   );
 END
 
-
 -- 풀이 3: LIMIT, OFFSET
 /* 
 <LIMIT>
@@ -62,6 +59,7 @@ LIMIT N(0부터 시작이니까 1번부터 따지면 N+1번임), M개
 함수 선언시 사용하는 변수를 바로 연산에 사용할 수 없다.
 BEGIN 안에 변수를 선언해야 한다 (DECLARE, SET)
 */
+               
 CREATE FUNCTION getNthHighestSalary(N INT)
 RETURNS INT
 BEGIN
