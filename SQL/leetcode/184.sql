6af219174a3e39ce573393d08ffcaeb9b27c094e
Jooseop Kim <65662520+js0221@users.noreply.github.com>
2021-04-02 17:40:49 +0900

    Update 184.sql

--- SQL/leetcode/184.sql
+++ SQL/leetcode/184.sql
@@ -1,3 +1,4 @@
+/* 풀이1 : Inner Join*/
 SELECT d.name AS department
      , e.name AS employee
      , e.salary
@@ -11,3 +12,21 @@ FROM employee e
                 AND e.salary = dh.max_salary
      -- Department 조인 (부서이름)
      INNER JOIN department d ON d.id = e.departmentid
+
+
+/* 풀이2 : 윈도우함수, 서브쿼리
+groupby를 window function의 partition을 사용하여 풀이
+SELECT에서 사용한 연산을 where절에서 바로 사용할 수 없기 때문에, 서브쿼리를 사용*/
+/* Write your T-SQL query statement below */
+SELECT ms.department
+     , ms.name AS Employee
+     , ms.salary
+FROM (
+    SELECT employee.name
+         , employee.salary
+         , department.name AS department
+         , MAX(salary) OVER (PARTITION BY departmentid) max_salary
+    FROM employee
+         INNER JOIN department ON employee.departmentid = department.id
+     ) ms
+WHERE ms.salary = ms.max_salary
