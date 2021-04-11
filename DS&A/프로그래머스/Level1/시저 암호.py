edf19bafb20e616ebaaee19d3984ba39ab616f7f
Jooseop Kim <65662520+js0221@users.noreply.github.com>
2021-04-11 20:34:05 +0900

    Update 시저 암호.py

--- DS&A/프로그래머스/Level1/시저 암호.py
+++ DS&A/프로그래머스/Level1/시저 암호.py
@@ -10,3 +10,15 @@ def solution(s, n):
             tmp = 32
         answer += chr(tmp)
     return answer
+
+
+# 다른 풀이
+def caesar(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+            
+    return "".join(s)
